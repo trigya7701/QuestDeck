00ef0e86e5b287407f7c354c4830a263a6c4a789
trigya7701 <rwikroy123@gmail.com>
2021-11-03 01:19:29 +0530

    Started with Home page

---
+++ components/navbar.php
@@ -0,0 +1,37 @@
+
+
+
+<nav class="navbar navbar-expand-lg   navbar-theme ">
+  <div class="container-fluid">
+    <a class="navbar-brand navbar-content" href="#">QuestDeck</a>
+    <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon navbar-toggle-btn"></span>
+    </button>
+    <div class="collapse navbar-collapse navbar-header" id="navbarSupportedContent">
+
+        <form class="d-flex navbar-form">
+        <input class="form-control me-2 navbar-searchbar" type="search" placeholder="Search Posts" aria-label="Search">
+        <button class="btn btn-light btn" type="submit">Search</button>
+      </form>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active navbar-content" aria-current="page" href="#">Home</a>
+          
+        </li>
+        <li class="nav-item">
+          <a class="nav-link navbar-content" href="#">Create Post</a>
+          
+        </li>
+        <li class="nav-item">
+          <a class="nav-link navbar-content" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link navbar-content" href="#">Profile</a>
+        </li>
+        
+        
+      </ul>
+      
+    </div>
+  </div>
+</nav>
---
+++ components/post.php
@@ -0,0 +1,40 @@
+
+
+<div class="card post-theme">
+  <h5 class="card-header">Featured</h5>
+  <div class="card-body ">
+    <h5 class="card-title">
+        <span class="d-inline-block text-truncate" style="max-width: 150px;">
+        this is first post on QuestDeck</span>
+    </h5>
+    
+    <a href="#" class="btn btn-danger btn-sm">View Post</a>
+  </div>
+</div>
+
+<div class="card post-theme">
+  <h5 class="card-header">Featured</h5>
+  <div class="card-body ">
+    <h5 class="card-title">
+        <span class="d-inline-block text-truncate" style="max-width: 150px;">
+        this is first post on QuestDeck</span>
+    </h5>
+    
+    <a href="#" class="btn btn-danger btn-sm">View Post</a>
+  </div>
+</div>
+<div class="card post-theme">
+  <h5 class="card-header">Featured</h5>
+  <div class="card-body ">
+    <h5 class="card-title">
+        <span class="d-inline-block text-truncate" style="max-width: 150px;">
+        this is first post on QuestDeck</span>
+    </h5>
+    
+    <a href="#" class="btn btn-danger btn-sm">View Post</a>
+  </div>
+</div>
+
+
+
+
